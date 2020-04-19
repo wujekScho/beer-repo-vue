5c95a66f53a9669880f7277d5c343d49d252d90b
Piotr Schodziński <schodziak@gmail.com>
2020-04-19 16:41:53 +0200

    AddRecipe extracted to separate component.

--- src/components/AddRecipe.vue
+++ src/components/AddRecipe.vue
@@ -1,11 +1,70 @@
 <template>
     <div>
-
+        <form v-if="yeasts.length">
+            <input placeholder="Name" v-model="name">
+            <input placeholder="Style" v-model="style">
+            <input min="0.1" placeholder="Gravity" step="0.1" type="number" v-model="gravity">
+            <input min="0.1" placeholder="Volume" step="0.1" type="number" v-model="volume">
+            <select v-model="yeastId">
+                <option :key="yeast.id" :value="yeast.id" v-for="yeast in yeasts">
+                    {{ yeast.name }}
+                </option>
+            </select>
+            <button @click="addRecipe">Add</button>
+        </form>
     </div>
 </template>
 
 <script>
-    export default {}
+    import api from "../api";
+
+    export default {
+        data() {
+            return {
+                yeasts: [],
+                yeastId: Number,
+                name: "",
+                style: "",
+                gravity: Number,
+                volume: Number
+            }
+        },
+        created() {
+            api
+                .get("yeasts")
+                .then(res => {
+                    this.yeasts = res.data.entity;
+                    this.yeastId = this.yeasts[0].id;
+                })
+                .catch(err => console.log(err));
+        },
+        methods: {
+            addRecipe: function (event) {
+                event.preventDefault();
+
+                let recipe = {
+                    name: this.name,
+                    gravity: this.gravity,
+                    volume: this.volume,
+                    style: this.style,
+                    yeastId: this.yeastId
+                }
+
+                api
+                    .post("brewings", recipe)
+                    .then(res => {
+                        if (res.status === 201) {
+                            this.$emit('recipe-added', res.data.entity);
+                        } else {
+                            console.log("Unknown status")
+                        }
+                    })
+                    .catch(err => {
+                        console.log("Unexpected error occurred" + err);
+                    })
+            }
+        }
+    }
 </script>
 
 <style scoped>
--- src/components/Recipes.vue
+++ src/components/Recipes.vue
@@ -4,38 +4,21 @@
                 v-bind:recipes="beerRecipes"
                 v-if="beerRecipes.length"
                 v-on:delete-recipe="deleteRecipe"/>
-
-        <form v-if="yeasts.length">
-            <input placeholder="Name" v-model="name">
-            <input placeholder="Style" v-model="style">
-            <input min="0.1" placeholder="Gravity" step="0.1" type="number" v-model="gravity">
-            <input min="0.1" placeholder="Volume" step="0.1" type="number" v-model="volume">
-            <select v-model="yeastId">
-                <option :key="yeast.id" :value="yeast.id" v-for="yeast in yeasts">
-                    {{ yeast.name }}
-                </option>
-            </select>
-            <button @click="addRecipe">Add</button>
-        </form>
-
+        <AddRecipe
+                v-on:recipe-added="addRecipe"/>
     </div>
 </template>
 
 <script>
     import api from "../api";
     import RecipesTable from "./RecipesTable";
+    import AddRecipe from "./AddRecipe";
 
     export default {
         name: "Recipes",
         data() {
             return {
-                beerRecipes: [],
-                yeasts: [],
-                yeastId: Number,
-                name: "",
-                style: "",
-                gravity: Number,
-                volume: Number
+                beerRecipes: []
             };
         },
         created() {
@@ -45,16 +28,10 @@
                     this.beerRecipes = res.data.entity;
                 })
                 .catch(err => console.log(err));
-            api
-                .get("yeasts")
-                .then(res => {
-                    this.yeasts = res.data.entity;
-                    this.yeastId = this.yeasts[0].id;
-                })
-                .catch(err => console.log(err));
         },
         components: {
-            RecipesTable: RecipesTable
+            RecipesTable: RecipesTable,
+            AddRecipe: AddRecipe
         },
         methods: {
             deleteRecipe: function (recipeId) {
@@ -78,27 +55,8 @@
                         this.beerRecipes.push(toDelete);
                     })
             },
-            addRecipe: function (event) {
-                event.preventDefault();
-                let recipe = {
-                    name: this.name,
-                    gravity: this.gravity,
-                    volume: this.volume,
-                    style: this.style,
-                    yeastId: this.yeastId
-                }
-                api
-                    .post("brewings", recipe)
-                    .then(res => {
-                        if (res.status === 201) {
-                            this.beerRecipes.push(res.data.entity);
-                        } else {
-                            console.log("Unknown status")
-                        }
-                    })
-                    .catch(err => {
-                        console.log("Unexpected error occurred" + err);
-                    })
+            addRecipe: function (recipe) {
+                this.beerRecipes.push(recipe);
             }
         }
     };
